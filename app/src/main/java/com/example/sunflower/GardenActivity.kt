5b37eed309a202bb2585b31d545a607d58979f08
honggi123 <89631493+honggi123@users.noreply.github.com>
2023-03-27 22:47:21 +0900

    메인 화면 탭 이동 관련 뷰, 어댑터 추가

--- app/src/main/java/com/example/sunflower/GardenActivity.kt
+++ app/src/main/java/com/example/sunflower/GardenActivity.kt
@@ -1,4 +1,16 @@
 package com.example.sunflower
 
-class GardenActivity {
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sunflower.R
+import com.example.sunflower.databinding.ActivityGardenBinding
+
+class GardenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView<ActivityGardenBinding>(this, R.layout.activity_garden)
+    }
+
 }
---
+++ app/src/main/java/com/example/sunflower/GardenFragment.kt
@@ -0,0 +1,21 @@
+package com.example.sunflower
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.sunflower.databinding.FragmentGardenBinding
+
+class GardenFragment :Fragment(){
+    private lateinit var binding: FragmentGardenBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentGardenBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
